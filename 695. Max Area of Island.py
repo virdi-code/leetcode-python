119228dde9bba45bb70ccc52227306fc045c4254
Rupinderveer Virdi <92199329+virdi-code@users.noreply.github.com>
2022-07-20 18:55:14 +0530

    Create 695. Max Area of Island.py

---
+++ 695. Max Area of Island.py
@@ -0,0 +1,21 @@
+# iterate over entire grid
+# call dfs on each
+# keep track of visited
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS,COLS = len(grid),len(grid[0])
+        visited = set()
+        
+        def dfs(r,c):
+            if (r <0 or r == ROWS or c < 0 or c == COLS or grid[r][c] == 0  or (r,c) in visited):
+                return 0
+            visited.add((r,c))
+            return 1 + dfs(r+1,c) + dfs(r,c+1) + dfs(r-1,c) + dfs(r,c-1)
+        area = 0
+        for i in range(ROWS):
+            for j in range(COLS):
+                area = max(area,dfs(i,j))
+                    
+        return area
