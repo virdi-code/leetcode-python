ab950919503e860197b58228cd0d5215d57e79fd
Rupinderveer Virdi <92199329+virdi-code@users.noreply.github.com>
2022-07-21 11:09:55 +0530

    Create 191. Number of 1 Bits.py

---
+++ 191. Number of 1 Bits.py
@@ -0,0 +1,11 @@
+# modulo and bit shift right.
+
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        
+        while n:
+            res += n%2
+            n = n >> 1
+        return res
